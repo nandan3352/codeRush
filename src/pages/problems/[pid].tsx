a9b2da81727b4c1bfc4dd8db5f1d9dd4de1aaab9
Pachhipulusu Nandan <nandan27072004@gmail.com>
2025-01-08 10:08:09 +0530

    commit

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -23,10 +23,6 @@ const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
 	);
 };
 export default ProblemPage;
-
-// fetch the local data
-//  SSG
-// getStaticPaths => it create the dynamic routes
 export async function getStaticPaths() {
 	const paths = Object.keys(problems).map((key) => ({
 		params: { pid: key },
@@ -37,9 +33,6 @@ export async function getStaticPaths() {
 		fallback: false,
 	};
 }
-
-// getStaticProps => it fetch the data
-
 export async function getStaticProps({ params }: { params: { pid: string } }) {
 	const { pid } = params;
 	const problem = problems[pid];
